c96c1d88ce1def1655aaf2ae8b94c838d48eccd0
Lâm Đức Anh Khoa <lamducanhkhoarubik@gmail.com>
2026-03-25 12:03:59 +0700

    Clean trash

--- Data/excel.py
+++
@@ -1,7 +0,0 @@
- task_id (Unique identifier)
-task_name (e.g., "Computer Architecture Lab 2", "Philosophy Reading")
-estimated_duration_minutes (Integer)
-deadline (Datetime)
-priority_level (Categorical: Low, Medium, High)
-cognitive_load (Numerical score representing how difficult the task is, to prevent scheduling three heavy tasks in a row).
-prerequisites (Array of other task_ids that must be completed first).
